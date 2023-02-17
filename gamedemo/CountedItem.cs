1a488b6649da9c3b741f0b389b1303a968f97bb1
Djilanie Soebhan <djilaniesoebhan@gmail.com>
2023-02-17 14:18:06 +0100

    Added the Item, CountedItem and Weapon classes. No methods have been made tho.

---
+++ gamedemo/CountedItem.cs
@@ -0,0 +1,13 @@
+﻿namespace gamedemo;
+
+public class CountedItem
+{
+    Item TheItem;
+    int Quantity;
+    
+    public CountedItem(Item item, int amount)
+    {
+        this.TheItem = item;
+        this.Quantity = amount;
+    }
+}
