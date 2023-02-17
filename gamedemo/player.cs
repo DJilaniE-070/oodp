957ee8edb753e4b66a1df2885cf2ab112e85550f
Ramazan <r.altintas_@hotmail.com>
2023-02-17 15:17:28 +0100

    Player class

---
+++ gamedemo/player.cs
@@ -0,0 +1,9 @@
+public class Player
+{
+    public string Name { get; set; }
+    public int CuHp { get; set; }
+    public int MaxHp { get; set; }
+    public int Gold { get; set; }
+    public int Xp { get; set; }
+    public int Level { get; set; }
+}
