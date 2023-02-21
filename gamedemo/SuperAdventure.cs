db45e1c1705a7aa060a6be5464e5ed26302c16dd
Djilanie Soebhan <djilaniesoebhan@gmail.com>
2023-02-21 15:46:08 +0100

    Added a base startup for the program that now works.

--- gamedemo/SuperAdventure.cs
+++ gamedemo/SuperAdventure.cs
@@ -2,15 +2,16 @@
 
 public class SuperAdventure
 {
-    public static void ThePlayer()
+    public static Player ThePlayer()
     {
         Player player = new Player();
-        player.Name = Console.ReadLine() ?? String.Empty;
+        player.Name = String.Empty;
         player.CuHp = 10;
         player.MaxHp = 10;
         player.Gold = 0;
         player.Xp = 0;
         player.Level = 1;
+        return player;
     }
 
     public static void CurrentMonster()
