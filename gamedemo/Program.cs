06f5f1d5941acaa4f52cea7dc754da2b56828734
Djilanie Soebhan <djilaniesoebhan@gmail.com>
2023-02-22 15:47:55 +0100

    You can now walk around the map. Quests and Monsters are not yet properly loaded. Need to fix it, monsters is less important then the quests.

--- gamedemo/Program.cs
+++ gamedemo/Program.cs
@@ -6,12 +6,30 @@ class Program
     {
         Player player = SuperAdventure.ThePlayer();
         Console.Write("Hello there. Enter your name: ");
-        player.Name = Console.ReadLine() ?? String.Empty;
+        //player.Name = Console.ReadLine() ?? String.Empty;
+        player.Name = "john doe";
 
         Console.WriteLine("Welcome to the world of Super Adventure, " + player.Name + "!");
 
         Location loc = World.LocationByID(1);
-        Console.WriteLine(loc.Name);
+        Console.WriteLine($"You're now at {loc.Name}. You can find the following things here:");
+        Console.WriteLine($"{loc.Description}");
+        Console.WriteLine($"The following quest is available:");
+        if (loc.QuestAvailableHere != null)
+        {
+            Console.WriteLine($"{loc.QuestAvailableHere.Name}");
+            Console.WriteLine($"{loc.QuestAvailableHere.Description}");
+        }
+        
+        Console.WriteLine($"The following monster is available:");
+        if (loc.MonsterLivingHere != null)
+        {
+            Console.WriteLine($"{loc.MonsterLivingHere.Name}");
+        }
+        
+        Console.WriteLine($"{loc.Compass()}");
+        
+        loc.MoveFromHome();
     }
 }
 
